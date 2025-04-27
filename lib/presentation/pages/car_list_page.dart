c676fa10c851905d6cbc0f4daf42e136d9962169
Ayush Sharma <165563915+iamayushsharmaa@users.noreply.github.com>
2025-04-27 13:51:58 +0530

    map

--- lib/presentation/pages/car_list_page.dart
+++ lib/presentation/pages/car_list_page.dart
@@ -1,8 +1,12 @@
 
 
+import 'package:bloc/bloc.dart';
 import 'package:car_rental_app/data/models/Car.dart';
+import 'package:car_rental_app/presentation/bloc/car_bloc.dart';
+import 'package:car_rental_app/presentation/bloc/car_states.dart';
 import 'package:car_rental_app/presentation/widgets/CarCard.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CarListPage extends StatelessWidget {
   final List<Car> cars = [
@@ -23,11 +27,28 @@ class CarListPage extends StatelessWidget {
         foregroundColor: Colors.black,
       ),
       backgroundColor: Colors.white,
-      body: ListView.builder(
-          itemCount: cars.length,
-          itemBuilder:(context, index){
-            return CarCard(car: cars[index]);
-          }
+      body: BlocBuilder<CarBloc, CarStates>(
+          builder: (context, state) {
+            if (state is CarsLoading){
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            else if(state is CarsLoaded){
+              return ListView.builder(
+                itemCount: cars.length,
+                itemBuilder: (context, index) {
+                  return CarCard(car: cars[index]);
+                },
+              );
+            }
+            else if(state is CarsError){
+              return Center(
+                child: Text('Error : ${state.message}'),
+              );
+            }
+            return Container();
+          },
       ),
     );
   }
